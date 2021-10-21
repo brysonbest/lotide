55d58e7cfba8b4f20838b434f088645b37989b6d
Bryson Best <brycekalden@gmail.com>
2021-10-21 23:45:28 +0000

    Linted.

--- eqArrays.js
+++ eqArrays.js
@@ -9,22 +9,22 @@ const assertEqual = function(actual, expected) {
 
 //function that checks for matching of arrays
 
-const eqArrays = function(array1, array2){
+const eqArrays = function(array1, array2) {
   //checks for initial matching of array items
-  for (i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i]){
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
   //checks for case where array2 may be longer
-  for (i = 0; i < array2.length; i++){
-    if (array2[i] !== array1[i]){
+  for (let i = 0; i < array2.length; i++) {
+    if (array2[i] !== array1[i]) {
       return false;
     }
   }
   //returns true if no mismatches found
   return true;
-}
+};
 
 //test cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
