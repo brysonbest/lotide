1054dfdfd8698ae1a7bfd360206ea8e2b67b96ba
Bryson Best <brycekalden@gmail.com>
2021-10-21 23:11:56 +0000

    Function that checks for the equivalence of two arrays.

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+// Function that compares two values and prints a message indicating whether or not they match
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//function that checks for matching of arrays
+
+const eqArrays = function(array1, array2){
+  //checks for initial matching of array items
+  for (i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  //checks for case where array2 may be longer
+  for (i = 0; i < array2.length; i++){
+    if (array2[i] !== array1[i]){
+      return false;
+    }
+  }
+  //returns true if no mismatches found
+  return true;
+}
+
+//test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
