c65068c9579a1872e9af1fdad58bd21283f86977
Bryson Best <bryson.best@gmail.com>
2021-10-27 16:47:51 -0700

    Added tests for specific letter cases.

--- countLetters.js
+++ countLetters.js
@@ -33,4 +33,7 @@ console.log(countLetters("Tomatoes are great"));
 
 assertEqual(countLetters("Tomatoes are great"), { T: 3, O: 2, M: 1, A: 3, E: 3, S: 1, R: 2, G: 1 }); //false as objects can't compare
 assertEqual(countLetters(""), {}); //false, same as previous
+assertEqual(countLetters("Red Buns").R, 1); //true
+assertEqual(countLetters("Purple People").P, 4); //true
+
 
