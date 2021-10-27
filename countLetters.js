8d8835ec2de25c967f238f0550430c9c9bb7800d
Bryson Best <bryson.best@gmail.com>
2021-10-27 16:26:23 -0700

    Made function work in outputting an object that lists the letters in a string, skipping spaces. Asserteq does not compare objects.

--- countLetters.js
+++ countLetters.js
@@ -8,6 +8,29 @@ const assertEqual = function(actual, expected) {
 };
 
 //Function that takes in a sentence (as a string) and then returns a count of each of the letters in that sentence.
-const countLetters = function(originalString){
+const countLetters = function(originalString) {
+  let stringCount = {};
+  if (!originalString) {
+    return stringCount;
+  }
+  for (let char of originalString) {
+    if (char !== ' ') {
+      let formatChar = char.toUpperCase();
+      if (stringCount[formatChar]) {
+        stringCount[formatChar] += 1;
+      } else {
+        stringCount[formatChar] = 1;
+      }
+    }
+  }
+  return stringCount;
+};
 
-}
+
+// Test code
+console.log(countLetters("Tomatoes are great"));
+
+
+assertEqual(countLetters("Tomatoes are great"), { T: 3, O: 2, M: 1, A: 3, E: 3, S: 1, R: 2, G: 1 }); //false as objects can't compare
+assertEqual(countLetters(""), {}); //false, same as previous
+
