1234d6e121269ae87913ec092c292b342dc828c1
Bryson Best <bryson.best@gmail.com>
2021-10-29 12:16:55 -0700

    Function that keeps collecting items from a provided array until the callback function provided returns a truthy value.

---
+++ takeUntil.js
@@ -0,0 +1,62 @@
+// Function that keeps collecting items from a provided array until the callback funciton provided returns a truthy value.
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if(callback(item)) {
+      return results;
+    } else {
+      results.push(item);
+    }
+  }
+  return results;
+};
+
+//Function that compares two arrays and prints a message indicating whether or not they match.
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+//function that checks for matching of arrays and returns a boolean
+
+const eqArrays = function(array1, array2) {
+  //checks if both arrays are blank;
+  if (!array1 && !array2) {
+    return true;
+  }
+  //checks if either array is blank; arrays do not have a falsey equivalent
+  if (!array1 || !array2) {
+    return false;
+  }
+  //check if arrays are equal in length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  //checks for initial matching of array items
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  //returns true if no mismatches found
+  return true;
+};
+
+//Test Code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); //true
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]); //true
+
+const data3 = [];
+assertArraysEqual(takeUntil(data3, x => x === 'yes'), []); //true
+
+const data4 = ['work', 5, 'hello', 11];
+assertArraysEqual(takeUntil(data4, x => x === 'work'), []); //true
+assertArraysEqual(takeUntil(data4, x => x === 11), ['work', 5, 'hello']); //true
