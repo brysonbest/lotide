a2997e1aab2f7da2f396de0568a0cfff8f3d7078
Bryson Best <bryson.best@gmail.com>
2021-11-11 14:12:47 -0800

    Refactored countOnlyTest to use mocha/chai.

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,22 +1,21 @@
 const countOnly = require('../countOnly');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-//test code
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+describe("#countOnly", () => {
+  let names = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason"," Salima","Fang","Joe"];
+  it(`returns mapped object with keys for count of true value names in an array`, () => {
+    assert.include(countOnly(names, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false}), {Jason: 1, Fang: 2});
+  });
+  it(`returns undefined if a name was not found on the list given`, () => {
+    assert.strictEqual(countOnly(names, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false})["Karima"], undefined);
+  });
+  it("returns undefined if a given name was on the list, but it had a false value", () => {
+    assert.strictEqual(countOnly(names, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false})["Agouhanna"], undefined);
+  });
+  it("returns an empty object for undefined", () => {
+    assert.include(countOnly, {});
+  });
+  it("returns an empty object for a blank array", () => {
+    assert.include(countOnly([]), {});
+  });
+});
