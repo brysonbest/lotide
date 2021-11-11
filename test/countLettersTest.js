5c7a443d721d7f5bc29ca7751897f31d304b5ed3
Bryson Best <bryson.best@gmail.com>
2021-11-11 13:59:27 -0800

    Refactored countLettersTest to use mocha and chai for testing.

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,10 +1,23 @@
 const countLetters = require('../countLetters');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-
-// Test code
-
-assertEqual(countLetters("Tomatoes are great"), { T: 3, O: 2, M: 1, A: 3, E: 3, S: 1, R: 2, G: 1 }); //false as objects can't compare
-assertEqual(countLetters(""), {}); //false, same as previous
-assertEqual(countLetters("Red Buns").R, 1); //true
-assertEqual(countLetters("Purple People").P, 4); //true
+describe("#countLetters", () => {
+  it(`returns 1 for number of letter R in "Red Buns"`, () => {
+    assert.strictEqual(countLetters("Red Buns").R, 1);
+  });
+  it(`returns 4 for number of letter P in "Purple People"`, () => {
+    assert.strictEqual(countLetters("Purple People").P, 4);
+  });
+  it("returns an object with keyed values for a String", () => {
+    assert.include(countLetters("Tomatoes are great"),{ T: 3, O: 2, M: 1, A: 3, E: 3, S: 1, R: 2, G: 1 });
+  });
+  it("returns an empty object for undefined", () => {
+    assert.include(countLetters, {});
+  });
+  it("returns an empty object for a blank string", () => {
+    assert.include(countLetters(""), {});
+  });
+  it("returns an empty object for a string of spaces", () => {
+    assert.include(countLetters("    "), {});
+  });
+});
