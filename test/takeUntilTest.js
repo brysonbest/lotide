15c740e0f9bb9299e36679306f5343c9ced4a5c8
Bryson Best <bryson.best@gmail.com>
2021-11-12 13:37:20 -0800

    Refactored to use chai/mocha for testing.

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,17 +1,20 @@
 const takeUntil = require('../takeUntil');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//Test Code
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); //true
-
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]); //true
-
-const data3 = [];
-assertArraysEqual(takeUntil(data3, x => x === 'yes'), []); //true
-
-const data4 = ['work', 5, 'hello', 11];
-assertArraysEqual(takeUntil(data4, x => x === 'work'), []); //true
-assertArraysEqual(takeUntil(data4, x => x === 11), ['work', 5, 'hello']); //true
+describe("#takeUntil", () => {
+  it(`returns [1, 2, 5, 7, 2] for callback x => x < 0 on original array [1, 2, 5, 7, 2, -1, 2, 4, 5]`, () => {
+    assert.deepEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5],x => x < 0), [1, 2, 5, 7, 2]);
+  });
+  it(`returns shortened array when first matching string found for callback x => x === ','.`, () => {
+    assert.deepEqual(takeUntil(["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"], x => x === ','), ["I've", "been", "to", "Hollywood"]);
+  });
+  it(`returns complete original array when number that meets callback criteria not present`, () => {
+    assert.deepEqual(takeUntil([5, 10, 15, 20],x => x < 0), [5, 10, 15, 20]);
+  });
+  it("returns empty array for empty array", () => {
+    assert.deepEqual(takeUntil([]), []);
+  });
+  it("returns error for undefined", () => {
+    assert.throw(takeUntil, Error);
+  });
+});
