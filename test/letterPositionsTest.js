4c32821887af9074fca2590cdc2facdd7d52d3f1
Bryson Best <bryson.best@gmail.com>
2021-11-12 13:18:45 -0800

    Refactored with mocha/chai for testing.

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,9 +1,20 @@
 const letterPositions = require('../letterPositions');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//test codes
-
-
-assertArraysEqual(letterPositions("Work it Out").t, [6, 10]); //true
-assertArraysEqual(letterPositions("lighthouse in the house").l, [0]); //true
-assertArraysEqual(letterPositions(""), {}); //true
+describe("#letterPositions", () => {
+  it(`returns [6,10] for "Work it Out" when looking for indices of the letter t`, () => {
+    assert.deepEqual(letterPositions("Work it Out").T, [6, 10]);
+  });
+  it(`returns undefined for "lighthouse" when looking for indices of the letter z`, () => {
+    assert.deepEqual(letterPositions("lighthouse").Z, undefined);
+  });
+  it(`returns an object {A: 0, 3, B: 1, 2} for the string "ABBA" `, () => {
+    assert.deepEqual(letterPositions("ABBA"), {A: [0, 3], B: [1 ,2]});
+  });
+  it("returns an empty object for an empty string", () => {
+    assert.deepEqual(letterPositions(""), {});
+  });
+  it("returns empty object for undefined", () => {
+    assert.deepEqual(letterPositions(), {});
+  });
+});
