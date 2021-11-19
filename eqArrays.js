875d0d40426f69254e9e70c64be5e5e92a7a3f57
Bryson Best <bryson.best@gmail.com>
2021-11-19 15:45:58 -0800

    Removed remaining test cases in functional code.

--- eqArrays.js
+++ eqArrays.js
@@ -28,8 +28,3 @@ const eqArrays = function(array1, array2) {
 };
 
 module.exports = eqArrays;
-
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
-
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
